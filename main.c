d7bd1ddf46cd6bd4a59ecb5337827896122959a7
Nikolay Panev <nikolay.n.panev@gmail.com>
2023-10-06 11:46:32 +0300

    Better OS and Error handling

--- main.c
+++ main.c
@@ -2,26 +2,59 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+
+/////////////////////////
+// PRE-PROCESSOR LOGIC //
+/////////////////////////
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+#pragma message ("COMPILING FOR NT SYSTEMS - OK")
+const char* OS = "WIN";
+#else
+#pragma message ("COMPILING FOR UNIX SYSTEMS - OK")
+const char* OS = "UNIX";
+#endif
+
 //////////////////////
 // DEFAULT ERR CODE //
 //////////////////////
 char errCode[] = "000";
 
-//////////////////////////
-// PREDECLARE FUNCTIONS //
-//////////////////////////
-void printArgs(int argc, char **argv);
+//////////////////////
+// CATCH ERROR CODE //
+//////////////////////
+
+int catchError(char *errCode){
+    if(errCode[0]!='0'){
+        char EndCmd[8] = "echo ";
+        EndCmd[5] = errCode[0];
+        EndCmd[6] = errCode[1];
+        EndCmd[7] = errCode[2];
+        printf("\n==========\nEXITED WITH CODE:\n");
+        system(EndCmd);
+        system("echo");
+        return 1;
+    }
+    printf("\n==========\n");
+    return 0;
+}
+
+///////////////////
+// VALIDATE ARGC //
+///////////////////
+int minArgc(int min,int argc){
+    if(argc<min) return 0;
+    return 1;
+}
 
 ////////////////////
 // FLAG FUNCTIONS //
 ////////////////////
+
         // HELP //
 void printHelp(){
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
-    system("type help.txt");
-#else
-    system("cat help.txt");
-#endif
+if(OS=="WIN")system("type help.txt");
+else         system("cat help.txt");
+
 }
 
         // MAKE DESCRIPTION FILE //
@@ -44,9 +77,8 @@ void makeDescription(int argc,char **argv){
 
         // MAKE DIRECTORY //
 void makeDir(int argc, char **argv, bool hasDesc){
-    char mkdir[] = "mkdir ~/";
+    const char* mkdir = "mkdir ~/";
 
-    
     if(hasDesc==1) {
         size_t mkdlen = strlen(mkdir);
         size_t arglen = strlen(argv[1]);
@@ -74,13 +106,13 @@ void makeDir(int argc, char **argv, bool hasDesc){
 
         // REMOVE DIRECTORY //
 void rmDir(int argc, char **argv){
-    const char *mkdir = "rm -r ~/";
-    size_t mkdlen = strlen(mkdir);
+    const char *rmdir = "rm -r ~/";
+    size_t rmdlen = strlen(rmdir);
     size_t arglen = strlen(argv[2]);
-    int cmdlen = mkdlen+1;
+    int cmdlen = rmdlen+1;
     cmdlen+= arglen;
     char cmd[cmdlen];
-    strcpy(cmd,mkdir);
+    strcpy(cmd,rmdir);
     strcat(cmd,argv[2]);
     system(cmd);
     printf("\n\tSuccessfully removed ~/%s \n",argv[2]);
@@ -91,11 +123,9 @@ void rmDir(int argc, char **argv){
 /////////////////////
 char *argOperationParser(int argc, char **argv){
     if(argc==1){
-        printf("Use -h for help\n");
         return "102";           //not enough arguments
     }
     bool hasDesc = 1;
-
     //parse flags
     if(argv[1][0]=='-' ){
         switch(argv[1][1]){
@@ -103,23 +133,20 @@ char *argOperationParser(int argc, char **argv){
                 printHelp();
                 return "000";
             case 'r':           //remove flag
+                if(!minArgc(3,argc)) return "102"; //not enough arguments
                 rmDir(argc,argv);
                 return "000";
-            case 'e':
+            case 'e':           //no description flag
+                if(!minArgc(3,argc)) return "102"; //not enough arguments
                 hasDesc=0;
                 break;
             default:            //invalid flag, error 101
-                printf("Invalid flag!\n");
-                printf("Use -h for help\n");
                 return "101";
         }
     }
     //if no flags, create the dir
     if(hasDesc==1){
-        if(argc<3){
-        printf("Use -h for help\n");
-        return "102";           //not enough arguments
-        }
+        if(!minArgc(3,argc)) return "102"; //not enough arguments
         makeDir(argc,argv,hasDesc);
         makeDescription(argc,argv);
     }
@@ -129,30 +156,11 @@ char *argOperationParser(int argc, char **argv){
     return "000";
 }
 
-// void printArgs(int argc, char **argv){
-//     printf("You have entered %d arguments:\n", argc-1);
-//     for (int i = 1; i < argc; i++) {
-//         printf("%s\n", argv[i]);
-//     }
-// }
-
 int main(int argc,char **argv){
     printf("\nEZDir v0.0.1\n==========\n\n");
     strcpy(errCode,argOperationParser(argc,argv));
 
     //Show error code if there is an error
-    if(errCode[0]!='0'){
-        char EndCmd[8] = "echo    ";
-        EndCmd[5] = errCode[0];
-        EndCmd[6] = errCode[1];
-        EndCmd[7] = errCode[2];
-        printf("\n==========\nEXITED WITH CODE:\n");
-        system(EndCmd);
-        system("echo");
-        return 1;
-    }
-    else{
-        printf("\n==========\n");
-    }
+    if(catchError(errCode) == 1)return 1;
     return 0;
 }
